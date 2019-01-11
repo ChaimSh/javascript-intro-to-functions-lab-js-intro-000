cbdce15fa874462a3036e6c6d16fa3aacd67468d
Chaim Shcherbina <rabbichaim770@gmail.com>
2019-01-11 03:06:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function whipser(string){
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+function sayHiToGrandma(string) {
+
+if(string.toLowerCase() === string){
+    return "I can\'t hear you!"
+  }
+else if(string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+else if(string === "I love you, Grandma."){
+  return "I love you, too."
+}  
+
+}
